42fbf8cc93541705a40c79bdcdd3127659987423
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-04-05 11:36:25 +0300

    scanner

--- scanner-dynamsoft/app-dynamsoft.js
+++ scanner-dynamsoft/app-dynamsoft.js
@@ -63,7 +63,7 @@ async function showScaner() {
       };
 
       await scanerObj.setUIElement(document.getElementById("barcode-scaner"));
-
+      await scanerObj.setResolution(300, 300);
       await scanerObj.show();
       document.getElementById("dce-video-container").style.display = "block";
     }
